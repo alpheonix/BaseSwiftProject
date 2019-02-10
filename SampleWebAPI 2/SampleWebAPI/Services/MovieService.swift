d6c8168b225c2a7834a485ca0c32a7479ab69f9d
alpheonix <arthurblanc98@gmail.com>
2019-02-10 19:07:17 +0100

    Update MovieService.swift

--- SampleWebAPI 2/SampleWebAPI/Services/MovieService.swift
+++ SampleWebAPI 2/SampleWebAPI/Services/MovieService.swift
@@ -1,30 +1,31 @@
 //
 //  MovieService.swift
-<<<<<<< HEAD
+
 //  HomePage
 //
 //  Created by Maxime ISTIN on 16/01/2019.
 //  Copyright © 2019 Maxime ISTIN. All rights reserved.
-=======
+
 //  SampleWebAPI
 //
 //  Created by Arthur BLANC on 05/02/2019.
 //  Copyright © 2019 Digipolitan. All rights reserved.
->>>>>>> e7785ccfd7af4920c193791b38d326d95fd15797
+
 //
 
 import Alamofire
 
-<<<<<<< HEAD
+
+
+   
+
+
 public class MovieService {
     
     public static let `default` = MovieService()
-    
-    private init(){
-        
+    private init( ){
     }
-    
-    public func getMovies(completion: @escaping ([Movie]) -> Void) {
+     public func getMovies(completion: @escaping ([Movie]) -> Void) {
         Alamofire.request("https://api.themoviedb.org/3/movie/now_playing?api_key=c2a65c4ec5c2e0b8847caec950444862&language=fr-FR").responseJSON{ (res) in
             guard let result = res.value as? [String:Any],
                   let resultat2 = result["results"] as? [[String:Any]] else {
@@ -37,13 +38,6 @@ public class MovieService {
                return Movie(json: elem)
             })
            completion(movie)
-=======
-
-public class MovieService {
-    
-    public static let `default` = MovieService()
-    private init( ){
-    }
     
     public func getOneMovie(completion: @escaping (DetailMovie) -> Void){
         SessionManager.default.request("https://api.themoviedb.org/3/movie/550?api_key=c2a65c4ec5c2e0b8847caec950444862&language=fr-FR").responseJSON { (res) in
@@ -52,7 +46,7 @@ public class MovieService {
             
             let movies = DetailMovie(titre:result["original_title"] as! String,description:result["overview"] as! String,poster:result["poster_path"] as! String,id:result["id"] as! Int,release_date: result["release_date"] as! String )
             completion(movies)
->>>>>>> e7785ccfd7af4920c193791b38d326d95fd15797
+
         }
     }
 }
